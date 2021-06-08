2f1cd79db3f88e83dbcde3acee36cc839d92b05e
seoyeon <hongssup0202@gmail.com>
2021-06-08 13:05:29 +0900

    Add functions managing Core Data

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    func getAllItems() {
+        do {
+            let items = try context.fetch(ToDoListItem.fetchRequest())
+        }
+        catch {
+            // error
+        }
+    }
+    
+    func createItem(name: String) {
+        let newItem = ToDoListItem(context: context)
+        newItem.name = name
+        newItem.createdAt = Date()
+        
+        do {
+            try context.save()
+        }
+        catch {
+            
+        }
+    }
+    
+    func deleteItem(item: ToDoListItem) {
+        context.delete(item)
+        
+        do {
+            try context.save()
+        }
+        catch {
+            
+        }
+    }
+    
+    func updateItem(item: ToDoListItem, newName: String) {
+        item.name = newName
+        
+        do {
+            try context.save()
+        }
+        catch {
+            
+        }
+    }
 }
 
